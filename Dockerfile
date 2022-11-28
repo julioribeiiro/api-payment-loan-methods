9c5db8a2c42a29a05fb9ddf66aca1fa4c37ca437
David Inojosa <david.inojosacc@gmail.com>
2022-11-28 01:44:39 -0300

    (feature) Added docker container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,16 @@
-FROM openjdk:17
-ARG JAR_FILE=out/artifacts/jcrm_dnio_project_2_jar/jcrm_dnio_project_2.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+# FROM openjdk:17
+# ARG JAR_FILE=out/artifacts/jcrm_dnio_project_2_jar/jcrm_dnio_project_2.jar
+# COPY ${JAR_FILE} app.jar
+# ENTRYPOINT ["java", "-jar", "/app.jar"]
+
+FROM eclipse-temurin:19-jdk-jammy
+
+WORKDIR /app
+
+COPY .mvn/ .mvn
+COPY mvnw pom.xml ./
+RUN ./mvnw dependency:resolve
+
+COPY src ./src
+
+CMD ["./mvnw", "spring-boot:run"]
--- src/main/java/com/jcrm/dnio/project2/jcrm_dnio_project_2/JcrmDnioProject2Application.java
+++ src/main/java/com/jcrm/dnio/project2/jcrm_dnio_project_2/JcrmDnioProject2Application.java
@@ -4,25 +4,26 @@ import com.jcrm.dnio.project2.jcrm_dnio_project_2.databaseSetup.DatabaseLink;
 import com.jcrm.dnio.project2.jcrm_dnio_project_2.importPayment.FetchData;
 import com.jcrm.dnio.project2.jcrm_dnio_project_2.importPayment.PaymentImporter;
 import com.jcrm.dnio.project2.jcrm_dnio_project_2.exportPayment.ToCSV;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JcrmDnioProject2Application {
 
 	public static void main(String[] args) {
-		final String databaseUrl = "jdbc:mysql://localhost:3306/jcrm_dnio";
-		final String xmlPath = "/Users/jmoraes/git/jcrm_dnio_project_2/src/main/java/com/jcrm/dnio/project2/jcrm_dnio_project_2/importPayment/data.xml";
-		final String csvUrl = "/Users/jmoraes/git/jcrm_dnio_project_2/src/main/java/com/jcrm/dnio/project2/jcrm_dnio_project_2/importPayment/paymentMethods.txt";
+//		final String databaseUrl = "jdbc:mysql://localhost:3306/jcrm_dnio";
+//		final String xmlPath = "/Users/jmoraes/git/jcrm_dnio_project_2/src/main/java/com/jcrm/dnio/project2/jcrm_dnio_project_2/importPayment/data.xml";
+//		final String csvUrl = "/Users/jmoraes/git/jcrm_dnio_project_2/src/main/java/com/jcrm/dnio/project2/jcrm_dnio_project_2/importPayment/paymentMethods.txt";
+//
+//		DatabaseLink databaseLink = new DatabaseLink(databaseUrl, "root", "123456789");
+//
+//		FetchData dataFrom = new FetchData(databaseLink, xmlPath);
+//		dataFrom.execute();
+//
+//		ToCSV dataTo = new ToCSV(databaseLink, csvUrl);
+//		dataTo.execute();
 
-		DatabaseLink databaseLink = new DatabaseLink(databaseUrl, "root", "123456789");
-
-		FetchData dataFrom = new FetchData(databaseLink, xmlPath);
-		dataFrom.execute();
-
-		ToCSV dataTo = new ToCSV(databaseLink, csvUrl);
-		dataTo.execute();
-
-//		SpringApplication.run(JcrmDnioProject2Application.class, args);
+		SpringApplication.run(JcrmDnioProject2Application.class, args);
 
 	}
 
